5f6ab1b3a436d941d9035f228188fc422711b4be
lyrahgames <markuspawellek@gmail.com>
2019-08-11 12:47:38 +0200

    Add color gradient

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 
+#include <array>
+#include <vector>
+
+using namespace std;
+
+using color = array<float, 4>;
+int width = 800;
+int height = 600;
+vector<color> pixel_buffer(width* height);
+
 int main() {
   // create the window
-  sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default,
+  sf::Window window({width, height}, "OpenGL", sf::Style::Default,
                     sf::ContextSettings(32));
   window.setVerticalSyncEnabled(true);
 
@@ -12,6 +22,13 @@ int main() {
 
   // load resources, initialize the OpenGL states, ...
   glClearColor(1, 0, 0, 1);
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const auto index = j * width + i;
+      const auto scale = static_cast<float>(j) / height;
+      pixel_buffer[index] = {scale, scale, scale, 1};
+    }
+  }
 
   // run the main loop
   bool running = true;
@@ -32,12 +49,9 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // draw...
+    glDrawPixels(width, height, GL_RGBA, GL_FLOAT, pixel_buffer.data());
 
     // end the current frame (internally swaps the front and back buffers)
     window.display();
   }
-
-  // release resources...
-
-  return 0;
 }
