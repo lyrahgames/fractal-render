20c091af56de5bb72b46e2551845094abc45b6d2
lyrahgames <markuspawellek@gmail.com>
2019-08-11 16:21:43 +0200

    Add rendering and morphing of Julia set

--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@ float x_max = origin_x + 0.5 * width;
 float y_max = origin_y + 0.5 * height;
 vector<color> pixel_buffer(screen_width* screen_height);
 
+complex<float> julia_coeff{};
+
 void draw_gradient() {
   for (int j = 0; j < screen_height; ++j) {
     for (int i = 0; i < screen_width; ++i) {
@@ -47,6 +49,27 @@ void draw_mandelbrot() {
       constexpr int max_it = 1 << 10;
       int it = 0;
       for (; (norm(z) < 4) && (it < max_it); ++it) z = z * z + c;
+
+      auto scale = log(1.f + it) / log(1.f + max_it);
+      pixel_buffer[index] = {scale, scale, scale, 1};
+    }
+  }
+}
+
+void draw_julia() {
+#pragma omp parallel for schedule(dynamic)
+  for (int j = 0; j < screen_height; ++j) {
+    for (int i = 0; i < screen_width; ++i) {
+      const auto index = j * screen_width + i;
+      const auto x = static_cast<float>(i) / screen_width;
+      const auto y = static_cast<float>(j) / screen_height;
+      complex<float> z{(x_max - x_min) * x + x_min,
+                       (y_max - y_min) * y + y_min};
+      // complex<float> c{0.5, -0.2};
+
+      constexpr int max_it = 1 << 10;
+      int it = 0;
+      for (; (norm(z) < 4) && (it < max_it); ++it) z = z * z + julia_coeff;
 
       auto scale = log(1.f + it) / log(1.f + max_it);
       pixel_buffer[index] = {scale, scale, scale, 1};
@@ -98,12 +121,12 @@ int main() {
         compute_viewport();
         glViewport(0, 0, screen_width, screen_height);
         pixel_buffer.resize(screen_width * screen_height);
-        draw_mandelbrot();
+        draw_julia();
       } else if (event.type == sf::Event::MouseWheelMoved) {
         height *= exp(-event.mouseWheel.delta * 0.05f);
         height = clamp(height, 1e-4f, 6.f);
         compute_viewport();
-        draw_mandelbrot();
+        draw_julia();
       }
     }
 
@@ -117,9 +140,18 @@ int main() {
       origin_y += delta_y;
 
       compute_viewport();
-      draw_mandelbrot();
+      draw_julia();
 
       // cout << "move\t" << delta_x << ", " ;
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+      const auto x = static_cast<float>(mouse_x) / screen_width * width + x_min;
+      const auto y =
+          static_cast<float>(mouse_y) / screen_height * height + y_min;
+      julia_coeff = {x, y};
+      compute_viewport();
+      draw_julia();
     }
 
     // clear the buffers
