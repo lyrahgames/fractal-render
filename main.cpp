7272bf261e6e41127efeca69d558999a5ef38560
lyrahgames <markuspawellek@gmail.com>
2019-08-11 11:43:55 +0200

    Use SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
+int main() {
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
-using namespace std;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
+    }
 
-int main() { cout << "Hello, World!\n"; }
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+  return 0;
+}
